65402d6fb9dbae43aaaea996bae1cf46c06ed604
ebanning <100447721+ebanning@users.noreply.github.com>
2022-11-01 18:19:11 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import pickle
+from flask import Flask, request, render_template
+app = Flask(__name__)
+model = pickle.load(open('model.sav', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict', methods = ['POST'])
+def predict():
+    features = [np.array([float(x) for x in request.form.values()])]
+    prediction = model.predict(features)
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text = 'House price prediction: ${}'.format(output))
+if __name__ == '__main__':
+    app.run(port = 5000, debug = True)
+
+
+# In[ ]:
+
+
+
+
